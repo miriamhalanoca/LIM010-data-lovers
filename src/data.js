94b70f092d8ec885b3773ff1c38ccc9a2cc93f4a
Alelizsanchez <Alelizsanchez@gmail.com>
2019-07-02 15:14:42 -0500

    verificando el main y pokemon js

--- src/data.js
+++ src/data.js
@@ -3,8 +3,17 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const POKEMON = () => {
-  return '';
+const pokedata = (pokemon) => {
+  let resultado1 = [];
+  for (let i = 0; i < pokemon.length; i++)
+    resultado1.push({
+      img: pokemon[i].img,
+      name: pokemon[i].name,
+
+    });
+  return resultado1;
 };
 
-window.POKEMON = POKEMON;
+window.pokemon = {
+  pokedata: pokedata,
+};
--- src/main.js
+++ src/main.js
@@ -3,28 +3,58 @@ const pagina1 = document.getElementById('pantalla1');
 const pagina2 = document.getElementById('pantalla2');
 const boton1 = document.getElementById('btn');
 
+
+/* inicio login */
 let contador = 0;
 
-boton1.addEventListener("click", (event) => {
-    event.preventDefault();
-    const contraseña = document.getElementById('pass').value;
-    const nombre1 = document.getElementById('nombre').value;
+boton1.addEventListener('click', (event) => {
+  event.preventDefault();
+  const contraseña = document.getElementById('pass').value;
+  const nombre1 = document.getElementById('nombre').value;
 
-    if (nombre1 === "LABORATORIA" && contraseña === "LABORATORIA") {
-        pagina1.classList.add("hide");
-        pagina2.classList.remove("hide");
+  if (nombre1 === 'LABORATORIA' && contraseña === 'LABORATORIA') {
+    pagina1.classList.add('hide');
+    pagina2.classList.remove('hide');
+  }
+  else {
+    contador = contador + 1;
+    document.getElementById('error').innerHTML = 'la contraseña es incorrecta'
+    if (contador === 3) {
+      document.getElementById('error').innerHTML = 'no se reconoce la contraseña'
     }
-    else {
-        contador = contador + 1;
-        document.getElementById('error').innerHTML = "la contraseña es incorrecta"
-        if (contador === 3) {
-          document.getElementById('error').innerHTML = "no se reconoce la contraseña"
-        }
-      }
-    
-    });
-const botonAtras = document.getElementById('atras');
-botonAtras.addEventListener("click", () => {
-    pagina2.classList.add("hide");
-    pagina1.classList.remove("hide");
+  }
+
 });
+const botonAtras = document.getElementById('atras');
+botonAtras.addEventListener('click', () => {
+  pagina2.classList.add('hide');
+  pagina1.classList.remove('hide');
+});
+
+/* fin Login*/
+
+/* mostrar data*/
+const datapokemon = document.getElementById('POKEMON.pokemon');
+const contenedorpoke = document.getElementById('contenedorpokemones');
+
+const mostrarpoke = (pokemon) => {
+  let resultado = ' ';
+  for (let i = 0; i < pokemon.length; i++)
+  let  llamar = `
+  <div> 
+  <img src = "${pokemon[i].img}"/>
+  <p> nombre= ${pokemon[i].name}</p>
+  <div>`;
+  resultado += llamar;
+}
+return resultado;
+};
+allpokemones.innerHTML = mostrarpoke(datapokemon)
+
+
+
+
+
+
+
+
