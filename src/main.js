4c41f57f2a718a1b446137341a7b04bec1bebe7c
Alelizsanchez <Alelizsanchez@gmail.com>
2019-07-18 09:18:25 -0500

    terminando de hacer las historias faltantes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 
+
 /* Manejo del DOM */
 const paginaUno = document.getElementById('pantallaUno');
 const paginaDos = document.getElementById('pantallaDos');
@@ -8,10 +9,11 @@ const encontrarPoke = document.getElementById('buscador');
 const ordenAbc = document.getElementById('ordenar');
 const tipos = document.getElementById('tipos');
 const direccionOrdenado = document.getElementById('direccion-ordenado');
+const pokefragil = document.getElementById('poke-debilidades');
 const seleccionarHuevo = document.getElementById('eclosionar');
+const menu = document.getElementById('menu-buscador');
 const pokedata = POKEMON.pokemon;
 let contador = 0;
-
 
 /* login */
 botonUno.addEventListener('click', (event) => {
@@ -21,9 +23,11 @@ botonUno.addEventListener('click', (event) => {
   if (usuario === '' && contraseña === '') {
     paginaUno.classList.add('hide');
     paginaDos.classList.remove('hide');
+    botonInicio.classList.remove('hide');
+    document.getElementById('botones').classList.remove('hide');
   } else {
     contador = contador + 1;
-    document.getElementById('error').innerHTML = 'la contraseña incorect';
+    document.getElementById('error').innerHTML = 'la contraseña incorecta';
     if (contador === 3) {
       document.getElementById('error').innerHTML = 'la contraseña es incorrecta vuelva a intentarlo mas tarde';
     }
@@ -34,6 +38,8 @@ botonUno.addEventListener('click', (event) => {
 botonInicio.addEventListener('click', () => {
   paginaDos.classList.add('hide');
   paginaUno.classList.remove('hide');
+  botonInicio.classList.add('hide');
+  document.getElementById('botones').classList.add('hide');
 });
 
 /* main */
@@ -45,7 +51,6 @@ const poke = (pokemon) => {
       <img class="imagenespoke" src="${pokemon[i].img}"/>
       <p class ="id">  ${pokemon[i].id}</p>
       <p class ="name"> ${pokemon[i].name}</p>
-      <p class="numero"> ${pokemon[i].num}</p>
       <p class="huevo"> ${pokemon[i].egg}</p>
       </div>`;
     almacenar += item;
@@ -56,14 +61,25 @@ contenedorPoke.innerHTML = poke(pokedata);
 
 /* filtrar todos los pokemones por tipos */
 tipos.addEventListener('change', (event) => {
+  document.getElementById('porcentaje').classList.add('hide');
   const tipoSeleccionado = event.target.value;
   let newarray = [];
   newarray = tipospoke(pokedata, tipoSeleccionado);
   contenedorPoke.innerHTML = poke(newarray);
 });
 
+/* filtrar todos los pokemones por debilidades */
+pokefragil.addEventListener('change', (event) => {
+  document.getElementById('porcentaje').classList.add('hide');
+  const fragil = event.target.value;
+  let newarraydebil = [];
+  newarraydebil = pokeDebilidad(pokedata, fragil);
+  contenedorPoke.innerHTML = poke(newarraydebil);
+});
+
 /* filtrar por acendente a descendente */
 direccionOrdenado.addEventListener('change', () => {
+  document.getElementById('porcentaje').classList.add('hide');
   const ordenarUno = direccionOrdenado.value;
   console.log(ordenarUno);
   let array = [];
@@ -71,28 +87,24 @@ direccionOrdenado.addEventListener('change', () => {
   contenedorPoke.innerHTML = poke(array);
 });
 
-/* ordenar de la A-Z */
+/* ordenar alfabeticamente */
 ordenAbc.addEventListener('change', () => {
-  if ('A-Z' === ordenAZ.value) {
-    const ordenar = filterpoke();
-    contenedorPoke.innerHTML = poke(ordenar);
-  }
-});
-/* ordenar de Z-A */
-ordenAbc.addEventListener('change', () => {
-  if ('Z-A' === ordenAZ.value) {
-    const ordenar1 = ordenarpokemones1();
-    contenedorPoke.innerHTML = poke(ordenar1);
-  }
+  document.getElementById('porcentaje').classList.add('hide');
+  const ordenarabcd = ordenAbc.value;
+  console.log(ordenarabcd);
+  let arrayabc = [];
+  arrayabc = ordenAlfabeticamente(pokedata, ordenarabcd);
+  contenedorPoke.innerHTML = poke(arrayabc);
 });
 
 /*  eclosionar huevos */
 seleccionarHuevo.addEventListener('change', (event) => {
+  document.getElementById('porcentaje').classList.remove('hide');
   const eggspoke = event.target.value;
   let array = [];
   array = huevosfilter(pokedata, eggspoke);
   contenedorPoke.innerHTML = poke(array);
-  document.getElementById('porcentaje').innerHTML = `el porcentaje de ${eggspoke} es : ${((array.length) / 151 * 100).toFixed(2)} %`;
+  document.getElementById('porcentaje').innerHTML = `El porcentaje de ${eggspoke} es : ${((array.length) / 151 * 100).toFixed(2)} %`;
 });
 
 /* buscar pokemones */
